eaea446775ace0e681a0b6dd422afc54bab8ec38
Kanishk Dogra <Kanishmain@gmail.com>
2024-02-07 19:50:36 -0500

    grammar and pic fixes

--- src/components/Intros/intro.jsx
+++ src/components/Intros/intro.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './intro.css'
-import pic from '../../assets/pic.jpeg'
+import pic from '../../assets/pic.png'
 export function Intro(){
     return(
         <section className="intro">
             <div className="introContent">
                 <span className="hello">hello</span>
                 <span className = "introText">I'm <span className = "introName">Kanishk</span></span> 
-                <p className = "introPara">IT Student currently stuying in 2<sup>nd</sup> Year at Nova Scotia Community College </p>
+                <p className = "introPara">IT Student currently studying in 2<sup>nd</sup> Year at Nova Scotia Community College </p>
             </div>
             <div className="pic">
                 {/* <img id="toggleTheme" class="theme-btn" src="assets/theme_light.png" alt="theme icon"/> */}
